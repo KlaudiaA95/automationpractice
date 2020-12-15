e7b56cf590668218c054bd8ad64545d49ca983db
klaudiaAleksandrzak <klaudia.aleksandrzak@pracuj.pl>
2020-12-15 23:48:22 +0100

    initial commit

---
+++ AutomationPractice2/Tests/Test.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationPractice2.Tests
+{
+    class Test
+    {
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
